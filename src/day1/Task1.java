64f672b87fcf10cb1855e12cd808d1261bd44fc5
PratheemaVijay <pratheemav7@gmail.com>
2022-03-14 23:24:38 +0530

    new class file

---
+++ src/day1/Task1.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class Task1 {
+
+	private void cliId() {
+		System.out.println("678");
+			}
+			private void cliName() {
+		System.out.println("HCL");
+		System.out.println("hcl1");
+			}
+			public static void main(String[] args) {
+			Task1 c = new Task1();
+				c.cliId();
+				c.cliName();
+					}
+}
