c668f137a7b331b1349734f4a0046743adde3d9d
PratheemaVijay <pratheemav7@gmail.com>
2022-03-16 10:50:46 +0530

    newly added

--- src/day1/Task1.java
+++ src/day1/Task1.java
@@ -9,6 +9,7 @@ public class Task1 {
 		System.out.println("HCL");
 		System.out.println("hcl1");
 		System.out.println("tcs");
+		System.out.println("pratheenma completed");
 			}
 			public static void main(String[] args) {
 			Task1 c = new Task1();
